bb5ca6fc3f3c1092f44bb2a07b5217c3287159bb
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-05-23 14:19:44 +0200

    Add a method to share location

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,38 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    //  Bing provides a URL format that supports opening a location
+	//  with latitude and longitude coordinates.
+    string baseUrl = "https://bing.com/maps/default.aspx?cp=";
 
-	public MainPage()
+    public string UserName { get; set; }
+
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnFindMeClicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		
 	}
+
+    // Gets the user’s name and shares their location.
+    private async Task ShareLocation()
+    {
+        UserName = UsernameEntry.Text;
+
+        var locationRequest = new GeolocationRequest(GeolocationAccuracy.Best);
+        var location = await Geolocation.GetLocationAsync(locationRequest);
+
+
+        await Share.RequestAsync(new ShareTextRequest
+        {
+            Subject = "Find me!",
+            Title = "Find me!",
+            Text = $"{UserName} is sharing their location with you",
+            Uri = $"{baseUrl}{location.Latitude}~{location.Longitude}"
+        });
+    }
 }
 
